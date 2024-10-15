a565e40225aff4dcf049adc42d1b1b14473db90f
Nomas <nomasx2000@gmail.com>
2024-10-16 00:57:41 +0300

    Initial Commit

---
+++ addons/main/config.cpp
@@ -0,0 +1,11 @@
+class CfgPatches {
+    class AET_AUX_Main {
+        author = "$STR_aet_aux_author";
+        name = "AET_AUX_Main";
+        url = "$STR_aet_aux_URL";
+        units[] = {};
+        weapons[] = {};
+        requiredAddons[] = {};
+        authors[] = {"Nomas / Redwan S. [AET]"};
+    };
+};
---
+++ mod.cpp
@@ -0,0 +1,12 @@
+name = "AET_AUX";
+picture = "AETlogo.paa";
+description = "";
+logo = "AETlogo.paa";
+logoOver = "AETlogo.paa";
+tooltip = "AET_AUX";
+tooltipOwned = "AET_AUX Owned";
+overview = "AET_AUX";
+author = "AET";
+overviewPicture = "AETlogo.paa";
+overviewText = "AET_AUX";
+overviewFootnote = "";
