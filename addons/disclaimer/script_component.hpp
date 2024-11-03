4eb2077060122ee9936ce56d655993b9cec20942
OverlordZorn <56258612+OverlordZorn@users.noreply.github.com>
2024-11-03 02:54:27 +0100

    Create script_component.hpp

---
+++ addons/disclaimer/script_component.hpp
@@ -0,0 +1,6 @@
+#define COMPONENT disclaimer
+#define COMPONENT_BEAUTIFIED Disclaimer       // This is being used for the Addon's Name and can be "My Addon Template Framework"
+
+
+#include "\z\aet_aux\addons\main\script_mod.hpp"
+#include "\z\aet_aux\addons\main\script_macros.hpp"
