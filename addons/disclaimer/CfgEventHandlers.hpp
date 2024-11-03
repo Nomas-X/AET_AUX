397fe583fa3bddd5fcda99d473d4f0d163bddd01
OverlordZorn <56258612+OverlordZorn@users.noreply.github.com>
2024-11-03 02:54:16 +0100

    Delete CfgEventHandlers.hpp

--- addons/disclaimer/CfgEventHandlers.hpp
+++
@@ -1,5 +0,0 @@
-class Extended_PreInit_EventHandlers {
-    class My_pre_init_event {
-        init = "call compile preprocessFileLineNumbers 'z\aet_aux\addons\disclaimer\XEH_preInit.sqf'";
-    };
-};
