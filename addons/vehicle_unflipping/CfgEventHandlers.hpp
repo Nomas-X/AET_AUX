5310b82306a4db34a779999d4f72a85c63afefac
Nomas <nomasx2000@gmail.com>
2024-11-08 14:57:24 +0300

    Renamed Addon Folder
    - Renamed addon folder from `unflipping` to `vehicle_unflipping`.
    - Updated all paths to match being in AET AUX mod.

---
+++ addons/vehicle_unflipping/CfgEventHandlers.hpp
@@ -0,0 +1,16 @@
+
+class Extended_PostInit_EventHandlers
+{
+    class vet_unflipping
+    {
+        serverInit = "call compile preprocessFileLineNumbers 'z\aet_aux\addons\vehicle_unflipping\XEH_postInitServer.sqf'";
+        clientInit = "call compile preprocessFileLineNumbers 'z\aet_aux\addons\vehicle_unflipping\XEH_postInitClient.sqf'";
+    };
+};
+class Extended_PreInit_EventHandlers
+{
+    class vet_unflipping
+    {
+        init = "call compile preprocessFileLineNumbers 'z\aet_aux\addons\vehicle_unflipping\XEH_preInit.sqf'";
+    };
+};
