48f88af2704678618027310a07add9b38db555bd
Nomas <nomasx2000@gmail.com>
2024-11-12 22:31:42 +0300

    Fixed paths for vehicle_unflipping addon
    - Updated paths in vehicle_unflipping addon to use aet instead of aet_aux.

--- addons/vehicle_unflipping/CfgEventHandlers.hpp
+++ addons/vehicle_unflipping/CfgEventHandlers.hpp
@@ -3,14 +3,14 @@ class Extended_PostInit_EventHandlers
 {
     class vet_unflipping
     {
-        serverInit = "call compile preprocessFileLineNumbers 'z\aet_aux\addons\vehicle_unflipping\XEH_postInitServer.sqf'";
-        clientInit = "call compile preprocessFileLineNumbers 'z\aet_aux\addons\vehicle_unflipping\XEH_postInitClient.sqf'";
+        serverInit = "call compile preprocessFileLineNumbers 'z\aet\addons\vehicle_unflipping\XEH_postInitServer.sqf'";
+        clientInit = "call compile preprocessFileLineNumbers 'z\aet\addons\vehicle_unflipping\XEH_postInitClient.sqf'";
     };
 };
 class Extended_PreInit_EventHandlers
 {
     class vet_unflipping
     {
-        init = "call compile preprocessFileLineNumbers 'z\aet_aux\addons\vehicle_unflipping\XEH_preInit.sqf'";
+        init = "call compile preprocessFileLineNumbers 'z\aet\addons\vehicle_unflipping\XEH_preInit.sqf'";
     };
 };
