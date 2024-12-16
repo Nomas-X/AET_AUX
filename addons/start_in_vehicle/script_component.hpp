4dfc8a6c9faa2be3cdd38404bf0db0e53d0554dd
Nomas <nomasx2000@gmail.com>
2024-12-16 23:05:01 +0300

    Creation of start_in_vehicle addon
    Start in vehicle function will be separated into its own addon as in the future it might include modules or more options, additionally there will be multiple functions and custom event handlers most likely.

---
+++ addons/start_in_vehicle/script_component.hpp
@@ -0,0 +1,8 @@
+#define COMPONENT start_in_vehicle
+
+// This is being used for the Addon's Name and can be "My Addon Template Framework"
+#define COMPONENT_BEAUTIFIED Start in Vehicle
+
+
+#include "\z\aet\addons\main\script_mod.hpp"
+#include "\z\aet\addons\main\script_macros.hpp"
