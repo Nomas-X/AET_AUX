3d43baec46b041ae7251147734a7cee8c8621fac
Esmeray/Sky <22858781+Esmeray6@users.noreply.github.com>
2024-12-20 15:24:00 +0500

    Add 100-round MG42 magazines

---
+++ addons/sog_modifications/CfgMagazines/belts.hpp
@@ -0,0 +1,16 @@
+class vn_mg42_50_mag;
+class vn_mg42_50_t_mag;
+
+class sog_modifications_mg42_100_mag: vn_mg42_50_mag {
+    count = 100;
+    mass = 60;
+    descriptionShort = "100Rnd. MG42 Drum. <br />Calibre: 7.92x57mm. <br />Used in MG42 machine gun";
+    displayName = "100Rnd. MG42 Drum";
+};
+
+class sog_modifications_mg42_100_t_mag: vn_mg42_50_t_mag {
+    count = 100;
+    mass = 60;
+    descriptionShort = "100Rnd. MG42 Tracer Drum. <br />Calibre: 7.92x57mm. <br />Used in MG42 machine gun";
+    displayName = "100Rnd. MG42 Drum (Tracer)";
+};
