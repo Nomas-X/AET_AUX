41a9905cea4cfb07c7550d23a58a4a6ec105e19b
Esmeray/Sky <22858781+Esmeray6@users.noreply.github.com>
2024-12-20 15:44:11 +0500

    aet_aux -> aet

--- addons/sog_modifications/script_component.hpp
+++ addons/sog_modifications/script_component.hpp
@@ -4,5 +4,5 @@
 #define COMPONENT_BEAUTIFIED SOG Modifications
 
 
-#include "\z\aet_aux\addons\main\script_mod.hpp"
-#include "\z\aet_aux\addons\main\script_macros.hpp"
+#include "\z\aet\addons\main\script_mod.hpp"
+#include "\z\aet\addons\main\script_macros.hpp"
