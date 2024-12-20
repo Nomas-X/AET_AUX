aa4f41469b497b181763677b6c03f52969303769
Esmeray/Sky <22858781+Esmeray6@users.noreply.github.com>
2024-12-20 15:25:11 +0500

    Commented out CfgFunctions.hpp for now, may be used later

---
+++ addons/sog_modifications/CfgFunctions.hpp
@@ -0,0 +1,14 @@
+// class CfgFunctions
+// {
+//     class SOG_MODIFICATIONS
+//     {
+//         class COMPONENT
+//         {
+
+//             file = PATH_TO_FUNC;
+
+//             class example { postInit = 1; };
+
+//         };
+//     };
+// };
