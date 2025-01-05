6d87819bcbe343a60ce9efe73b53a4bfca2552de
Nomas <nomasx2000@gmail.com>
2025-01-05 06:47:05 +0300

    Replaced addons with PBOs
    - Replaced external addons with their PBO version as there is no need for us to unpack them and this causes less headache down the line.

--- addons/vehicle_unflipping/script_component.hpp
+++
@@ -1,8 +0,0 @@
-#define COMPONENT vehicle_unflipping
-
-// This is being used for the Addon's Name and can be "My Addon Template Framework"
-#define COMPONENT_BEAUTIFIED Vehicle Unflipping
-
-
-#include "\z\aet\addons\main\script_mod.hpp"
-#include "\z\aet\addons\main\script_macros.hpp"
