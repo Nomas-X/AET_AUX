9ed073386d4931b06b3fa3a2a7c6f15c027503d4
Nomas <nomasx2000@gmail.com>
2025-05-31 04:54:21 +0300

    Removing `plan_importer` addon
    - The `plan_importer` addon is removed as it is now its own mod as of https://github.com/Nomas-X/AET_Plan_Importer

--- addons/plan_importer/script_component.hpp
+++
@@ -1,8 +0,0 @@
-#define COMPONENT plan_importer
-
-// This is being used for the Addon's Name and can be "My Addon Template Framework"
-#define COMPONENT_BEAUTIFIED Plan Importer
-
-
-#include "\z\aet\addons\main\script_mod.hpp"
-#include "\z\aet\addons\main\script_macros.hpp"
