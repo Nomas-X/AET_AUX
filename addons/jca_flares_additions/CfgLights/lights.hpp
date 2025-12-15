923afd038042a7bb7963c59fb58d91f167568a16
Esmeray/Sky <22858781+Esmeray6@users.noreply.github.com>
2025-12-15 11:23:21 +0500

    Honestly I'm not sure what the fuck I did here except tweak numbers that visually do *nothing*

--- addons/jca_flares_additions/CfgLights/lights.hpp
+++ addons/jca_flares_additions/CfgLights/lights.hpp
@@ -1,15 +1,15 @@
 class JCA_HandFlare_Light_Base;
 class AET_JCA_HandFlare_Light_White: JCA_HandFlare_Light_Base
 {
-    color[] = {0.5, 0.5, 0.5, 1};
-    diffuse[] = {0.8, 0.8, 0.8, 1};
-    ambient[] = {0,8, 0,8, 0,8, 1};
+    color[] = {0.3, 0.3, 0.3, 0.5};
+    diffuse[] = {0.25, 0.25, 0.25, 0.5};
+    ambient[] = {0,3, 0,3, 0,3, 0.5};
 };
 
 class JCA_SignalFlare_Light_Base;
 class AET_JCA_SignalFlare_Light_White: JCA_SignalFlare_Light_Base
 {
-    color[] = {0.5, 0.5, 0.5, 1};
+    color[] = {0.3, 0.3, 0.3, 1};
     diffuse[] = {0.8, 0.8, 0.8, 1};
     ambient[] = {0.5, 0.5, 0.5, 1};
 };
@@ -18,5 +18,5 @@ class JCA_HandFlare_Light_Sparks_Base;
 class AET_JCA_HandFlare_Light_Sparks_White: JCA_HandFlare_Light_Sparks_Base
 {
     color[] = {0.3, 0.3, 0.3, 1};
-    diffuse[] = {0.8, 0.8, 0.8, 1};
+    diffuse[] = {0.2, 0.2, 0.2, 1};
 };
