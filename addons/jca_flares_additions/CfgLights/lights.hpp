d749175123f149fecb26fdb0e45c93761418c9a5
Esmeray/Sky <22858781+Esmeray6@users.noreply.github.com>
2025-12-19 21:22:42 +0500

    Final config settings

--- addons/jca_flares_additions/CfgLights/lights.hpp
+++ addons/jca_flares_additions/CfgLights/lights.hpp
@@ -1,22 +1,24 @@
 class JCA_HandFlare_Light_Base;
 class AET_JCA_HandFlare_Light_White: JCA_HandFlare_Light_Base
 {
-    color[] = {0.3, 0.3, 0.3, 0.5};
-    diffuse[] = {0.25, 0.25, 0.25, 0.5};
-    ambient[] = {0,3, 0,3, 0,3, 0.5};
+    color[] = {0.2, 0.2, 0.2};
+    diffuse[] = {2, 2, 2};
+    ambient[] = {2, 2, 2};
+    flareSize = 3.5;
 };
 
 class JCA_SignalFlare_Light_Base;
 class AET_JCA_SignalFlare_Light_White: JCA_SignalFlare_Light_Base
 {
-    color[] = {0.3, 0.3, 0.3, 1};
-    diffuse[] = {0.8, 0.8, 0.8, 1};
-    ambient[] = {0.5, 0.5, 0.5, 1};
+    color[] = {0.2, 0.2, 0.2};
+    diffuse[] = {2, 2, 2};
+    ambient[] = {2, 2, 2};
+    flareSize = 2;
 };
 
 class JCA_HandFlare_Light_Sparks_Base;
 class AET_JCA_HandFlare_Light_Sparks_White: JCA_HandFlare_Light_Sparks_Base
 {
-    color[] = {0.3, 0.3, 0.3, 1};
-    diffuse[] = {0.2, 0.2, 0.2, 1};
+    color[] = {0.1, 0.1, 0.1};
+    // diffuse[] = {0.2, 0.2, 0.2, 1};
 };
