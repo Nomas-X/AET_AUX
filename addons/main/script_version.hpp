5670cfee0a09dca7aa3fb30ad6c32c8c9bfed9b6
Nomas <78245023+Nomas-X@users.noreply.github.com>
2026-05-01 14:34:48 +0300

    Removed `tcf_optre_fixes` addon
    - The original mod has addressed the issues directly, this is no longer needed.

--- addons/main/script_version.hpp
+++ addons/main/script_version.hpp
@@ -1,7 +1,7 @@
 #define MAJOR 3
-#define MINOR 1
-#define PATCH 2
-#define BUILD 910
+#define MINOR 2
+#define PATCH 0
+#define BUILD 911
 
 // #define VERSION MACROS
 #define VERSION MAJOR.MINOR
--- addons/tcf_optre_fixes/script_component.hpp
+++
@@ -1,8 +0,0 @@
-#define COMPONENT tcf_optre_fixes
-
-// This is being used for the Addon's Name and can be "My Addon Template Framework"
-#define COMPONENT_BEAUTIFIED The Carver Findings Operation Trebuchet AET Fixes
-
-
-#include "\z\aet\addons\main\script_mod.hpp"
-#include "\z\aet\addons\main\script_macros.hpp"
