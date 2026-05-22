55a6e680c500e25ec2babbbfa5664bb9cc6698ac
Nomas <78245023+Nomas-X@users.noreply.github.com>
2026-05-22 17:01:27 +0300

    New ace_fixes addon
    - Fix ACE Speed Limiter for aircrafts.
    - Add removeActionFromZeus function.
    
    This addon is temporary until the main ACE version is updated to include the fixes / features.

---
+++ addons/ace_fixes/CfgFunctions.hpp
@@ -0,0 +1,22 @@
+class CfgFunctions
+{
+	class ace_interact_menu
+	{
+		class Misc
+		{
+			file = PATH_TO_FUNC;
+
+			class removeActionFromZeus {};
+		};
+	};
+
+	class ace_vehicles
+	{
+		class Misc
+		{
+			file = PATH_TO_FUNC;
+
+			class autoThrottle {};
+		};
+	};
+};
--- addons/main/script_version.hpp
+++ addons/main/script_version.hpp
@@ -1,7 +1,7 @@
 #define MAJOR 3
-#define MINOR 2
+#define MINOR 3
 #define PATCH 1
-#define BUILD 917
+#define BUILD 921
 
 // #define VERSION MACROS
 #define VERSION MAJOR.MINOR
